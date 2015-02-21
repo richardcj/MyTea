b19633cd0ec961c47a850907808a39b900af1505
richardcj <enjoymyself.honest@163.com>
2015-02-21 11:54:53 +0800

    mytea init
    mytea project

---
+++ src/com/zhong/adapter/MainFragmentPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.zhong.adapter;
+
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 主界面viewpager适配器
+ * 
+ * @author 仲丛旭
+ * 
+ */
+public class MainFragmentPagerAdapter extends FragmentPagerAdapter {
+	private final String TAG = "MainFragmentPagerAdapter";
+	private List<Fragment> list = null;
+
+	public MainFragmentPagerAdapter(FragmentManager fm, List<Fragment> list) {
+		super(fm);
+		this.list = list;
+	}
+
+	@Override
+	public Fragment getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return list.get(arg0);
+	}
+
+	@Override
+	public Object instantiateItem(View container, int position) {
+		Log.i(TAG, "==instantiateItem()");
+		return super.instantiateItem(container, position);
+	}
+	
+	/*@Override
+	public Object instantiateItem(ViewGroup container, int position) {
+		container.addView(list.get(position),0);
+		return list.get(position);
+	}*/
+
+	@Override
+	public int getCount() {
+		return list.size();
+	}
+
+}
